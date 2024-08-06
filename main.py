316c0e2057b39c111c8f4cc6fb61e556f1548d71
jasonthuynh <jasonhuynh512@gmail.com>
2024-08-06 11:15:25 -0500

    base proj commit

--- main.py
+++ main.py
@@ -51,7 +51,54 @@ def room():
     room = session.get("room")
     if room == None or session.get("name") == None or room not in rooms:
         return redirect(url_for("home"))
-    return render_template("room.html")
+    return render_template("room.html", roomCode=room, messages=rooms[room]["messages"])
+
+ 
+@socketio.on("message")
+def message(data):
+    room = session.get("room")
+    if room not in room:
+        return
+    
+    content = {
+        "name": session.get("name"),
+        "message": data["data"]
+    }
+    send(content, to=room)
+    rooms[room]["messages"].append(content)
+    print(f"{session.get('name')} said: {data['data']}")
+
+
+
+@socketio.on("connect")
+def connect(auth):
+    room = session.get("room")
+    name = session.get("name")
+    if not room or not name:
+        return
+    if room not in rooms:
+        leave_room(room)
+        return
+
+    join_room(room)
+    send({"name": name, "message": "has entered the room"}, to=room)
+    rooms[room]["members"] += 1
+    print(f"{name} joined room {room}")
+
+@socketio.on("disconnect")
+def disconnect():
+    room = session.get("room")
+    name = session.get("name")
+    leave_room(room)
+
+    if room in rooms:
+        rooms[room]["members"] -= 1
+        if rooms[room]["members"] <= 0:
+            del rooms[room]
+
+    send({"name": name, "message": "has left the room"}, to=room)
+    print(f"{name} left room {room}")
+
 
 if __name__ == "__main__":
     socketio.run(app, debug=True)
